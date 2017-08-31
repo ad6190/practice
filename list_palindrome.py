c4e6e47779317bb41460041f10a052b1e5d9b65e
RLS009 <aditibhatnagar6190@gmail.com>
2017-08-31 14:53:21 +0530

    remove dups from sorted list

--- list_palindrome.py
+++ list_palindrome.py
@@ -49,14 +49,20 @@ class LinkedList():
 	def reverse_recursion(self, node):
 		if node.next == None:
 			self.head = node
-			return 
-		
+			return
 		self.reverse_recursion(node.next)
 		temp = node.next
 		temp.next = node
 		node.next = None
 
 
+	def remove_dups(self):
+		current = self.head
+		while(current.next):
+			if current.next.val == current.val:
+				current.next = current.next.next
+			else:
+				current = current.next
 
 if __name__ == '__main__':
 	linkedlist = LinkedList()
@@ -64,7 +70,13 @@ if __name__ == '__main__':
 	linkedlist.insert(2)
 	linkedlist.insert(3)
 	linkedlist.insert(4)
+	linkedlist.insert(4)
+	linkedlist.insert(4)
+	linkedlist.insert(4)
+	linkedlist.insert(4)
 	linkedlist.print_all()
 	linkedlist.reverse_list() 
 	linkedlist.reverse_recursion(linkedlist.head)
+	linkedlist.print_all()
+	linkedlist.remove_dups()
 	linkedlist.print_all()
